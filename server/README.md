4e72ea86be3dd0504d4e653a96c5b257426f316a
Banggi Bima <angetaiayam@gmail.com>
2023-06-06 21:40:42 +0700

    Initial commit

---
+++ server/README.md
@@ -0,0 +1,10 @@
+Query SQL nya di bawah
+
+```sql
+CREATE TABLE todos (
+	id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	title VARCHAR(255),
+	description VARCHAR(255),
+	created_at TIMESTAMP
+);
+```
---
+++ server/index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "your_username",
+  password: "your_password",
+  database: "your_database_name",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ status: 200, message: "Succeed" });
+});
+
+app.get("/items", (req, res) => {
+  const sql = "SELECT * FROM todos";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error executing query:", err);
+      res.status(500).json({ error: "Error executing query" });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+app.listen(port, () => {
+  console.log("Server started on port 5000");
+});
